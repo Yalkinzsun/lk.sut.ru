9f47fdc45ab87687ab20105809f4ad577c0e37e6
Kirill Skorobogatov <ya.star-lord@ya.ru>
2022-03-21 23:49:49 +0300

    updated

--- bonch/main.py
+++ bonch/main.py
@@ -16,6 +16,8 @@ def get_data_from_session_if_possible():
     journal_group = ""
     journal_sem = 0
     journal_disp = ""
+    day_index_in_headings = 0
+
     if 'journal_group' in session:
         journal_group = session['journal_group']
 
@@ -24,7 +26,11 @@ def get_data_from_session_if_possible():
 
     if 'journal_disp' in session:
         journal_disp = session['journal_disp']
-    return journal_group, journal_sem, journal_disp
+
+    if 'day_index_in_headings' in session:
+        day_index_in_headings = session['day_index_in_headings']
+
+    return journal_group, journal_sem, journal_disp, day_index_in_headings
 
 
 def get_visits_data_from_db():
@@ -32,19 +38,23 @@ def get_visits_data_from_db():
                 ("Практика 2", "01.09"), ("Лекция 2", "01.09"), ("Практика 3", "01.09"),
                 ("Лекция 3", "01.09"), ("Практика 4", "01.09"), ("Лекция 4", "01.09"))
     data = (
-        ((1, "Хабельников Никита Андреевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "Н", ""), (uuid.uuid4(), "", ""),
+        ((1, "Хабельников Никита Андреевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "Н", ""),
+         (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
 
-        ((2, "Хорошева Евгения Онеговна", "староста"), ("", "", ""), ("", "", ""), (uuid.uuid4(), "Н", ""), (uuid.uuid4(), "", ""),
+        ((2, "Хорошева Евгения Онеговна", "староста"), ("", "", ""), ("", "", ""), (uuid.uuid4(), "Н", ""),
+         (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
 
-        ((3, "Земляникина Полина Ахметовна", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "У", ""), (uuid.uuid4(), "", ""),
+        ((3, "Земляникина Полина Ахметовна", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "У", ""),
+         (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
 
-        ((4, "Ежегодов Дмитрий Сергеевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "Б", ""), (uuid.uuid4(), "", ""),
+        ((4, "Ежегодов Дмитрий Сергеевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "Б", ""),
+         (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""))
     )
@@ -56,33 +66,63 @@ def get_works_data_from_db():
                 ("ПР 2", "до 01.09"), ("ЛР 2", "до 01.09"), ("ЛР 3", "до 01.09"),
                 ("ЛР 4", "до 01.09"))
     data = (
-        ((105, "Хабельников Никита Андреевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st0", ""), (uuid.uuid4(), "st6", ""),
+        ((105, "Хабельников Никита Андреевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st0", ""),
+         (uuid.uuid4(), "st6", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
 
-        ((243, "Хорошева Евгения Онеговна", "староста"), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st1", ""), (uuid.uuid4(), "st4", ""),
+        ((243, "Хорошева Евгения Онеговна", "староста"), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st1", ""),
+         (uuid.uuid4(), "st4", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
 
-        ((512, "Земляникина Полина Ахметовна", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st2", ""), (uuid.uuid4(), "st5", ""),
+        ((512, "Земляникина Полина Ахметовна", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st2", ""),
+         (uuid.uuid4(), "st5", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
 
-        ((346, "Ежегодов Дмитрий Сергеевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st3", ""), (uuid.uuid4(), "", ""),
+        ((346, "Ежегодов Дмитрий Сергеевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st3", ""),
+         (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""))
     )
     return headings, data
 
-def get_students_work_data_from_db():
 
-    return True
+def get_attestation_data_from_db():
+    headings = (("Студент", ""), ("", ""), ("", ""), ("Практика 1", "01.09"), ("Лекция 1", "01.09"),
+                ("Практика 2", "01.09"), ("Лекция 2", "01.09"), ("Практика 3", "01.09"),
+                ("Лекция 3", "01.09"), ("Практика 4", "01.09"), ("Лекция 4", "01.09"))
+    data = (
+        ((1, "Хабельников Никита Андреевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "Н", ""),
+         (uuid.uuid4(), "", ""),
+         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
+         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
+
+        ((2, "Хорошева Евгения Онеговна", "староста"), ("", "", ""), ("", "", ""), (uuid.uuid4(), "Н", ""),
+         (uuid.uuid4(), "", ""),
+         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
+         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
+
+        ((3, "Земляникина Полина Ахметовна", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "У", ""),
+         (uuid.uuid4(), "", ""),
+         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
+         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
+
+        ((4, "Ежегодов Дмитрий Сергеевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "Б", ""),
+         (uuid.uuid4(), "", ""),
+         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
+         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""))
+    )
+    return headings, data
+
+
 
 
 
 def get_column_index_for_highlighting(headings):
     today = date.today()
 
-    if not today.strftime("%d.%m.%Y") in headings:
+    if today.strftime("%d.%m.%Y") in headings:
         return 4
     else:
-        return 1
+        return 5
 
 
 def is_it_necessary_to_insert(column_index):
@@ -94,7 +134,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/start_journal', methods=('GET', 'POST'))
+@app.route('/start-journal', methods=('GET', 'POST'))
 def start_journal():
     headings = get_visits_data_from_db()[0]
 
@@ -107,7 +147,7 @@ def start_journal():
         #     row[selected_day] = request.form[f'{data.index(row)}']
         #     inserting = False
 
-    journal_group, journal_sem, journal_disp = get_data_from_session_if_possible()
+    journal_group, journal_sem, journal_disp, index = get_data_from_session_if_possible()
 
     if 'journal_group' not in session and 'journal_sem' not in session and 'journal_disp' not in session:
         return render_template('start_journal.html', journal_group=journal_group,
@@ -116,71 +156,72 @@ def start_journal():
     else:
         index_of_column_for_highlighting = get_column_index_for_highlighting(headings)
         if index_of_column_for_highlighting != 1:
+            session['day_index_in_headings'] = index_of_column_for_highlighting
             if is_it_necessary_to_insert(index_of_column_for_highlighting):
-                return redirect(url_for('insert_to_journal', index=index_of_column_for_highlighting))
+                return redirect(url_for('insert_to_journal', day_index=index_of_column_for_highlighting))
             else:
-                return redirect(url_for('show_journal', index=index_of_column_for_highlighting))
+                return redirect(url_for('show_journal'))
 
 
-@app.route('/journal/insert_<int:index>', methods=('GET', 'POST'))
-def insert_to_journal(index):
+@app.route('/journal/insert/<int:day_index>', methods=('GET', 'POST'))
+def insert_to_journal(day_index):
+    journal_group, journal_sem, journal_disp, index = get_data_from_session_if_possible()
     headings, data = get_visits_data_from_db()
     truncated_headings = (headings[0], headings[index])
     truncated_data = []
     for row in data:
         truncated_data.append((row[0], row[index]))
 
-    journal_group, journal_sem, journal_disp = get_data_from_session_if_possible()
     if journal_group == '':
         return redirect(url_for('start_journal'))
     else:
 
         return render_template('insert_to_journal.html',
-                           index=index,
-                           headings=truncated_headings,
-                           data=tuple(truncated_data),
-                           journal_group=journal_group,
-                           journal_sem=journal_sem,
-                           inserting=True,
-                           journal_disp=journal_disp)
+                               index=day_index,
+                               headings=truncated_headings,
+                               data=tuple(truncated_data),
+                               journal_group=journal_group,
+                               journal_sem=journal_sem,
+                               inserting=True,
+                               journal_disp=journal_disp)
 
 
-@app.route('/journal/<int:index>')
-def show_journal(index):
+@app.route('/journal')
+def show_journal():
     headings, data = get_visits_data_from_db()
-    journal_group, journal_sem, journal_disp = get_data_from_session_if_possible()
+    journal_group, journal_sem, journal_disp, index = get_data_from_session_if_possible()
     if journal_group == '':
         return redirect(url_for('start_journal'))
     else:
         return render_template('journal.html',
-                           index=index,
-                           headings=headings,
-                           data=data,
-                           journal_group=journal_group,
-                           journal_sem=journal_sem,
-                           journal_disp=journal_disp)
+                               index=index,
+                               headings=headings,
+                               data=data,
+                               journal_group=journal_group,
+                               journal_sem=journal_sem,
+                               journal_disp=journal_disp)
 
 
 @app.route('/journal/works')
 def students_works():
     headings, data = get_works_data_from_db()
-    journal_group, journal_sem, journal_disp = get_data_from_session_if_possible()
+    journal_group, journal_sem, journal_disp, index = get_data_from_session_if_possible()
     if journal_group == '':
         return redirect(url_for('start_journal'))
     else:
         return render_template('students_works.html',
-                           headings=headings,
-                           data=data,
-                           journal_group=journal_group,
-                           journal_sem=journal_sem,
-                           journal_disp=journal_disp)
+                               headings=headings,
+                               data=data,
+                               journal_group=journal_group,
+                               journal_sem=journal_sem,
+                               journal_disp=journal_disp)
 
 
 @app.route('/journal/works/<string:works_id>', methods=('GET', 'POST'))
 def check_students_work(works_id):
     # headings, data = get_students_work_data_from_db()
-    journal_group, journal_sem, journal_disp = get_data_from_session_if_possible()
-    file_name="IST032m pr1.pdf"
+    journal_group, journal_sem, journal_disp, index = get_data_from_session_if_possible()
+    file_name = "IST032m pr1.pdf"
     name_of_work = "Практическая работа #1"
     deadline = "до 15 апреля 2022"
     student = "Иванова И. И."
@@ -198,51 +239,40 @@ def check_students_work(works_id):
     else:
 
         return render_template('check_students_work.html',
-                           # headings=headings,
-                           # data=data,
-                           works_id=works_id,
-                           file_name=file_name,
-                           name_of_work= name_of_work,
-                           deadline=deadline,
-                           student=student,
-                           status=status,
-                           comment=comment,
-                           time_of_comment=time_of_comment,
-                           next_work_id=next_work_id,
-                           journal_group=journal_group,
-                           journal_sem=journal_sem,
-                           journal_disp=journal_disp)
+                               # headings=headings,
+                               # data=data,
+                               works_id=works_id,
+                               file_name=file_name,
+                               name_of_work=name_of_work,
+                               deadline=deadline,
+                               student=student,
+                               status=status,
+                               comment=comment,
+                               time_of_comment=time_of_comment,
+                               next_work_id=next_work_id,
+                               journal_group=journal_group,
+                               journal_sem=journal_sem,
+                               journal_disp=journal_disp)
+
+
+@app.route('/journal/attestation')
+def show_attestation():
+    headings, data = get_attestation_data_from_db()
+    journal_group, journal_sem, journal_disp, index = get_data_from_session_if_possible()
+    if journal_group == '':
+        return redirect(url_for('start_journal'))
+    else:
+        return render_template('journal_attestation.html',
+                               headings=headings,
+                               data=data,
+                               journal_group=journal_group,
+                               journal_sem=journal_sem,
+                               journal_disp=journal_disp)
 
 
 @app.route('/journal/statistic')
 def show_statistic():
-    journal_group, journal_sem, journal_disp = get_data_from_session_if_possible()
-    students, tasks, data = 15, 10, []
-    for student in range(students):
-        data.append(list(stats.randint(1, 5).rvs(tasks - 2)) + [0, 0])
-
-    x = [f"Задание {i}" for i in range(1, tasks)]
-    y = [f"Студент {i}  " for i in reversed(range(1, students))]
-
-    fig = go.Figure(go.Heatmap(
-        z=data,
-        colorscale=[
-            [0, '#F9F9F9'],
-            [0.25, '#08C827'],
-            [0.50, '#F9FC69'],
-            [0.75, '#EFA12B'],
-            [1, '#F62323'],
-        ],
-        x=x,
-        y=y,
-        colorbar=dict(
-            tick0=0,
-            tickmode='array',
-            tickvals=['pusto', '1', '2', '3', '4']
-        ),
-    ))
-    fig.update_xaxes(side="top")
-    fig.show()
+    journal_group, journal_sem, journal_disp, index = get_data_from_session_if_possible()
     return render_template('statistic.html',
                            journal_group=journal_group,
                            journal_sem=journal_sem,
