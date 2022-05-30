7fa0db8fb572a4354fd02c51f586d2afc9996467
Kirill Skorobogatov <ya.star-lord@ya.ru>
2022-05-30 19:22:48 +0300

    updated

--- sut/main.py
+++ sut/main.py
@@ -5,7 +5,7 @@ from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
 from flask_login import login_required, current_user
 from DB.database import get_visits_data_from_db, insert_visits_data_to_db
-
+import numpy as np
 import sqlite3
 import sys
 import random
@@ -18,6 +18,7 @@ from flask import Blueprint
 from flask import send_from_directory
 
 main = Blueprint('main', __name__)
+
 
 def get_data_from_session_if_possible():
     journal_group = ""
@@ -40,34 +41,40 @@ def get_data_from_session_if_possible():
     return journal_group, journal_sem, journal_disp, day_index_in_headings
 
 
+student_works_data = [
+    [[105, "Хабельников Никита Андреевич", ""], ["", "", ""], ["", "", ""], [uuid.uuid4(), "st2", ""],
+     [uuid.uuid4(), "st1", ""],
+     [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""]],
+
+    [[243, "Хорошева Евгения Онеговна", "староста"], ["", "", ""], ["", "", ""], [uuid.uuid4(), "st5", ""],
+     [uuid.uuid4(), "st0", ""],
+     [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""]],
+
+    [[512, "Земляникина Полина Ахметовна", ""], ["", "", ""], ["", "", ""], [uuid.uuid4(), "st2", ""],
+     [uuid.uuid4(), "st4", ""],
+     [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""]],
+
+    [[346, "Ежегодов Дмитрий Сергеевич", ""], ["", "", ""], ["", "", ""], [uuid.uuid4(), "st3", ""],
+     [uuid.uuid4(), "", ""],
+     [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""]],
+
+    [[746, "Белозубкина Анна Михайловна", ""], ["", "", ""], ["", "", ""], [uuid.uuid4(), "", ""],
+     [uuid.uuid4(), "", ""],
+     [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""], [uuid.uuid4(), "", ""]]
+]
+
+
 def get_works_data_from_db():
-    headings = (("Студент", ""), ("", ""), ("", ""), ("ПР 1", "до 01.09"), ("ЛР 1", "до 01.09"),
-                ("ПР 2", "до 01.09"), ("ЛР 2", "до 01.09"), ("ЛР 3", "до 01.09"),
-                ("ЛР 4", "до 01.09"))
-    data = (
-        ((105, "Хабельников Никита Андреевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st0", ""),
-         (uuid.uuid4(), "st6", ""),
-         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
-
-        ((243, "Хорошева Евгения Онеговна", "староста"), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st1", ""),
-         (uuid.uuid4(), "st4", ""),
-         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
-
-        ((512, "Земляникина Полина Ахметовна", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st2", ""),
-         (uuid.uuid4(), "st5", ""),
-         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
-
-        ((346, "Ежегодов Дмитрий Сергеевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "st3", ""),
-         (uuid.uuid4(), "", ""),
-         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""))
-    )
+    headings = (("Студент", ""), ("", ""), ("", ""), ("ПР 1", "до 09.09"), ("ПР 2", "до 11.09"),
+                ("ПР 3", "до 13.09"), ("ПР 4", "до 15.09"), ("ПР 5", "до 17.09"))
+    data = student_works_data
     return headings, data
 
 
 def get_attestation_data_from_db():
-    count_of_works = 15
-    count_of_practical_classes = 15
-    count_of_all_classes = 30
+    count_of_works = 5
+    count_of_practical_classes = 5
+    count_of_all_classes = 10
     count_of_discipline_points = 100
 
     count_of_points_for_three = 65
@@ -78,59 +85,78 @@ def get_attestation_data_from_db():
                 ("Посещаемость", "общая"), ("Кол-во баллов", ""), ("Пром. аттестация", "доступно с 11.11"),
                 ("Рек. оценка", ""), ("Экзамен", "доступно с 10.01"))
     data = (
-        ((1, "Хабельников Никита Андреевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "6", ""),
-         (uuid.uuid4(), 8, ""),
-         (uuid.uuid4(), 12, ""), (uuid.uuid4(), 48, ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
+        ((1, "Хабельников Никита Андреевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "1", ""),
+         (uuid.uuid4(), 1, ""),
+         (uuid.uuid4(), 3, ""), (uuid.uuid4(), 26, ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
 
-        ((2, "Хорошева Евгения Онеговна", "староста"), ("", "", ""), ("", "", ""), (uuid.uuid4(), "7", ""),
-         (uuid.uuid4(), 9, ""),
-         (uuid.uuid4(), 14, ""), (uuid.uuid4(), 65, ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
+        ((2, "Хорошева Евгения Онеговна", "староста"), ("", "", ""), ("", "", ""), (uuid.uuid4(), "0", ""),
+         (uuid.uuid4(), 1, ""),
+         (uuid.uuid4(), 1, ""), (uuid.uuid4(), 22, ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
 
-        ((3, "Земляникина Полина Ахметовна", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "9", ""),
-         (uuid.uuid4(), 4, ""),
-         (uuid.uuid4(), 12, ""), (uuid.uuid4(), 91, ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
+        ((3, "Земляникина Полина Ахметовна", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "1", ""),
+         (uuid.uuid4(), 1, ""),
+         (uuid.uuid4(), 3, ""), (uuid.uuid4(), 35, ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
 
-        ((4, "Ежегодов Дмитрий Сергеевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "10", ""),
-         (uuid.uuid4(), 5, ""),
-         (uuid.uuid4(), 16, ""), (uuid.uuid4(), 75, ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
+        ((4, "Ежегодов Дмитрий Сергеевич", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "0", ""),
+         (uuid.uuid4(), 0, ""),
+         (uuid.uuid4(), 1, ""), (uuid.uuid4(), 8, ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
+         (uuid.uuid4(), "", ""), (uuid.uuid4(), "", "")),
+        ((5, "Белозубкина Анна Михайловна", ""), ("", "", ""), ("", "", ""), (uuid.uuid4(), "0", ""),
+         (uuid.uuid4(), 2, ""),
+         (uuid.uuid4(), 3, ""), (uuid.uuid4(), 6, ""), (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""),
          (uuid.uuid4(), "", ""), (uuid.uuid4(), "", ""))
     )
     return headings, data, count_of_works, count_of_practical_classes, count_of_all_classes, count_of_discipline_points, count_of_points_for_three, count_of_points_for_four, count_of_points_for_five
 
 
 def get_statistic_data_for_plotting():
-    students = [f'Студент {i}' for i in range(1, 16)]
+    students = [
+        'Хабельников Н. А.',
+        'Хорошева Е. О.',
+        'Земляникина П. А.',
+        'Ежегодов Д. С.',
+        'Белозубкина А. М.'
+    ]
 
-    tasks, data = 4, []
-    for student in range(len(students) + 1):
-        data.append(list(stats.randint(1, 4).rvs(tasks)) + [0, 0])
-    z_heatmap = data
+    # tasks, data = 4, []
+    # for student in range(len(students) + 1):
+    #     print(list(stats.randint(1, 4).rvs(tasks)))
+    #     data.append(list(stats.randint(1, 4).rvs(tasks)) + [0, 0])
+    # z_heatmap = data
 
-    x_stacked = ['Задание 1', 'Задание 2', 'Задание 3', 'Задание 4', 'Задание 5']
-    y1_stacked = [15, 12, 10, 12, 3]
-    y2_stacked = [0, 3, 5, 3, 12]
+    z_heatmap = [[3, 0, 0, 0, 0, 0, 0],
+                 [1, 0, 0, 0, 0, 0, 0],
+                 [1, 1, 0, 0, 0, 0, 0],
+                 [1, 0, 0, 0, 0, 0, 0],
+                 [1, 1, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0]]
+
+    x_stacked = ['ПР 1', 'ПР 2', 'ПР 3', 'ПР 4', 'ПР 5']
+    y1_stacked = [2, 0, 0, 0, 0]
+    y2_stacked = [2, 2, 0, 0, 0]
+    y3_stacked = [1, 3, 5, 5, 5]
     num_of_students = len(students)
-    x_bar_visits = [7, 4, 5, 6, 7, 4, 7, 6, 3, 7, 2, 4, 1, 5, 7]
-    minimum_allowed_number_of_visits = 5
+    x_bar_visits = [1, 0, 1, 0, 0]
+    minimum_allowed_number_of_visits = 4
     num_of_points_for_three = 65
     num_of_points_for_four = 75
     num_of_points_for_five = 85
-    x_bar_points = [110, 75, 50, 73, 89, 15, 87, 12, 89, 56, 70, 36, 77, 45, 30]
+    x_bar_points = [6, 8, 35, 22, 26]
 
-    return z_heatmap, x_stacked, y1_stacked, y2_stacked, num_of_students, x_bar_visits, students, \
+    return z_heatmap, x_stacked, y1_stacked, y2_stacked, y3_stacked, num_of_students, x_bar_visits, students, \
            minimum_allowed_number_of_visits, num_of_points_for_three, num_of_points_for_four, \
            num_of_points_for_five, x_bar_points
 
 
 def get_course_data_from_db():
     data = (
-        ('3267275', 'Задание 1', 'Постановка задачи (опционально)', '01.09.2022', '14.09.2022', '10', 15, (
+        ('3267275', 'Задание 1', 'Провести сравнительный анализ современных систем управления базами данных', '01.09.2022', '09.09.2022', '5', 15, (
             '12_09_2013_8_b.jpg', 'IST032m pr1.pdf', 'python_file.py', 'ЛР2 Nginx (p.1) Скоробогатов КД ИСТ-032м.docx',
             'Эссе бд.zip'), 1),
-        ('4573473', 'Задание 2', 'Постановка задачи (опционально)', '01.09.2022', '14.09.2022', '10', 15, (), 1),
+        ('4573473', 'Задание 2', 'Ознакомиться с особенностями организации web-приложений на выбранном фреймворке', '01.09.2022', '11.09.2022', '7', 15, (), 1),
     )
     return data
 
@@ -216,18 +242,16 @@ def insert_to_journal():
 
                 visits_data.append([
                     id_statement,
-                    i+1,
+                    i + 1,
                     id_teacher,
                     id_class,
                     str(status)[2],
                     mark
                 ])
 
-
             insert_visits_data_to_db(visits_data)
 
             return redirect(url_for('main.show_journal'))
-
 
         return render_template('insert_to_journal.html',
                                index=index,
@@ -279,16 +303,24 @@ def check_students_work(work_id):
     journal_group, journal_sem, journal_disp, index = get_data_from_session_if_possible()
     file_name = "IST032m pr1.pdf"
     name_of_work = "Практическая работа #1"
-    deadline = "до 15 апреля 2022"
-    student = "Иванова И. И."
+    deadline = "до 09 сентября 2022"
+    student = "Хорошева Е. О."
     status = "st3"
-    comment = "Очень большой доинный комментарий к прошлой работе, кторая на самом деле была выполнена крайне " \
-              "небрежно и к тому же совершенно неверно "
+    comment = "Оформление работы не соотвествует ГОСТу, ошибка в результатах проведённого эксперимента, остуствует " \
+              "список используемых источников"
     time_of_comment = "12 апреля 2022 14:10"
     next_work_id = ''
 
     if request.method == 'POST':
-        next_work_id = "12345"
+        student_works_data[1][3][1] = 'st2'
+        print(student_works_data[1][3][1])
+        file_name = "IST032m pr1.pdf"
+        name_of_work = "2"
+        deadline = "до 09 сентября 2022"
+        student = "Хорошева Е. О."
+        status = "st3"
+        comment = ""
+        time_of_comment = ""
 
     if journal_group == '':
         return redirect(url_for('main.start_journal'))
@@ -303,9 +335,9 @@ def check_students_work(work_id):
                                deadline=deadline,
                                student=student,
                                status=status,
+                               next_work_id=next_work_id,
                                comment=comment,
                                time_of_comment=time_of_comment,
-                               next_work_id=next_work_id,
                                journal_group=journal_group,
                                journal_sem=journal_sem,
                                journal_disp=journal_disp)
@@ -376,16 +408,17 @@ def show_attestation():
 def show_statistic():
     journal_group, journal_sem, journal_disp, index = get_data_from_session_if_possible()
 
-    z_heatmap, x_stacked, y1_stacked, y2_stacked, num_of_students, x_bar_visits, students, \
+    z_heatmap, x_stacked, y1_stacked, y2_stacked, y3_stacked, num_of_students, x_bar_visits, students, \
     minimum_allowed_number_of_visits, num_of_points_for_three, num_of_points_for_four, \
     num_of_points_for_five, x_bar_points = get_statistic_data_for_plotting()
+
     graphs = [
         dict(
             data=[
                 dict(
-                    x=[f"Задание {i}" for i in range(1, len(z_heatmap[0]))],
-                    y=[f"Студент {i}  " for i in reversed(range(1, len(z_heatmap)))],
-                    z=z_heatmap,
+                    x=x_stacked,
+                    y=students[::-1],
+                    z=np.array(z_heatmap),
                     type='heatmap',
                     colorscale=[
                         [0, '#F9F9F9'],
@@ -397,6 +430,9 @@ def show_statistic():
                 ),
             ],
             layout=dict(
+                margin=dict(
+                    l=150
+                ),
                 title='Соответствие плановому графику выполнения контрольных заданий',
                 xaxis=dict(
                     ticks='',
@@ -409,17 +445,34 @@ def show_statistic():
                 dict(
                     x=x_stacked,
                     y=y1_stacked,
-                    name='Все сданные работы',
+                    name='Кол-во приянтых работ',
                     textposition='auto',
-                    text=y1_stacked,
+                    text=['2', '0', '0', '0', '0'],
+                    marker=dict(
+                        color='#25C0D9'
+                    ),
                     type='bar'
                 ),
                 dict(
                     x=x_stacked,
                     y=y2_stacked,
+                    name='Все сданные работы',
+                    textposition='auto',
+                    text=[4, 2, 0, 0, 0],
+                    marker=dict(
+                        color='#2571D9'
+                    ),
+                    type='bar'
+                ),
+                dict(
+                    x=x_stacked,
+                    y=y3_stacked,
                     name='Кол-во всех студентов',
                     textposition='auto',
-                    text=[num_of_students for task in x_stacked],
+                    text=[5, 5, 5, 5, 5],
+                    marker=dict(
+                        color='#CCCCCC'
+                    ),
                     type='bar'
                 ),
             ],
@@ -439,7 +492,7 @@ def show_statistic():
                     text=x_bar_visits,
                     orientation='h',
                     marker=dict(
-                        color='#669933'
+                        color='#A3C285'
                     )
                 ),
                 dict(
@@ -462,6 +515,11 @@ def show_statistic():
                 ),
             ],
             layout=dict(
+                xaxis=dict(tickformat=',d'),
+                margin=dict(
+                    l=150,
+                    pad=20
+                ),
                 title='Посещаемость практических занятий',
                 showlegend=False,
             )
@@ -477,7 +535,7 @@ def show_statistic():
                     text=x_bar_points,
                     orientation='h',
                     marker=dict(
-                        color='#CCCCCC'
+                        color='#FFC266'
                     )
                 ),
                 dict(
@@ -536,6 +594,11 @@ def show_statistic():
                 ),
             ],
             layout=dict(
+                xaxis=dict(tickformat=',d'),
+                margin=dict(
+                    l=150,
+                    pad=20
+                ),
                 title='Количество набранных баллов',
                 showlegend=False,
             )
